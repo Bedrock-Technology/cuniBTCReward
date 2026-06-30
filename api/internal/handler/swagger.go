7048189c21b2022d4531907c93c34a913c87b4a0
xynor <ace.xinxuwang@gmail.com>
2026-06-30 22:20:00 +0800

    f

--- api/internal/handler/swagger.go
+++ api/internal/handler/swagger.go
@@ -3,7 +3,7 @@ package handler
 import (
 	"net/http"
 
-	"github.com/swaggest/swgui/v5emb"
+	"github.com/swaggest/swgui/v5cdn"
 	"github.com/zeromicro/go-zero/rest"
 )
 
@@ -20,7 +20,7 @@ func RegisterSwaggerHandlers(server *rest.Server) {
 }
 
 func SwaggerHandler() http.HandlerFunc {
-	swagger := v5emb.New(
+	swagger := v5cdn.New(
 		"cuniBTC",
 		"/docs/cuniBTCReward.json",
 		"",
