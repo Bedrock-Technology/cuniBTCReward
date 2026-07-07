b2408cc33ccccf3fe5808e2a95510daa9e86f7dd
xynor <ace.xinxuwang@gmail.com>
2026-07-07 20:11:18 +0800

    f

--- api/internal/logic/epochlistlogic.go
+++ api/internal/logic/epochlistlogic.go
@@ -103,7 +103,9 @@ epoch_unclaimed AS (
 	JOIN top_epoches te ON te.contract = s.vault
     WHERE drr.chain_id = ? 
       AND drr.deleted_at IS NULL 
-	  AND drr.claim_at < FROM_UNIXTIME(te.lockup_start + te.lockup_period)
+	  AND (
+	       drr.claimed = 0 OR drr.claim_at < FROM_UNIXTIME(te.lockup_start + te.lockup_period)
+	  )
     GROUP BY te.contract, te.epoch
 ),
 airdrop_agg AS (
