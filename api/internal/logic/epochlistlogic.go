b67f7c70a07c9d7a3a0d0114c8e30e089be4e8c5
xynor <ace.xinxuwang@gmail.com>
2026-07-07 20:35:32 +0800

    f

--- api/internal/logic/epochlistlogic.go
+++ api/internal/logic/epochlistlogic.go
@@ -103,7 +103,7 @@ epoch_unclaimed AS (
 	JOIN top_epoches te ON te.contract = s.vault
     WHERE drr.chain_id = ? 
       AND drr.deleted_at IS NULL
-	  AND drr.create_block_time < te.lockup_start + te.lockup_period
+	  AND drr.create_block_time < FROM_UNIXTIME(te.lockup_start + te.lockup_period)
 	  AND drr.claimed = 0 
     GROUP BY te.contract, te.epoch
 ),
